132514404cc58a8899a14809439fdf40492c8200
GhostWriter2023 <154560842+GhostWriter2023@users.noreply.github.com>
2024-02-21 16:16:39 -0500

    Changes for Task 2.2.4
    -removed log.txt & index.html files.
    -created "public" directory and relocated "documentation.html" file.
    -created 2 GET routes: 1) returns a JSON object; and 2) returns a text response.
    -created an express.static fn to serve “documentation.html” file.
    -created a fn to Log all requests using Morgan.

---
+++ index.js
@@ -0,0 +1,53 @@
+const express = require("express"),
+  morgan = require("morgan");
+
+const app = express();
+
+let topTenMovies = [
+  {
+    title: "Mission: Impossible Dead Reckoning",
+  },
+  {
+    title: "Guardians of the Galaxy",
+  },
+  {
+    title: "Jack Reacher",
+  },
+  {
+    title: "The Equalizer",
+  },
+  {
+    title: "Top Gun",
+  },
+  {
+    title: "Gladiator",
+  },
+  {
+    title: "Extraction",
+  },
+  {
+    title: "Lord of the Rings",
+  },
+  {
+    title: "Black Widow",
+  },
+  {
+    title: "Avengers: Endgame",
+  },
+];
+
+app.use(morgan("common"));
+
+app.get("/movies", (req, res) => {
+  res.json(topTenMovies);
+});
+
+app.get("/", (req, res) => {
+  res.send("myFlix web page is in development!");
+});
+
+app.use(express.static('public'));
+
+app.listen(8080, () => {
+  console.log("Your app is listening on port 8080.");
+});
