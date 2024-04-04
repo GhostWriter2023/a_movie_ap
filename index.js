0d96ebbcb949423b02b1e72560eda4d6313e8b75
GhostWriter2023 <154560842+GhostWriter2023@users.noreply.github.com>
2024-04-04 18:48:24 -0400

    Authentication & Authorization
    -Installed packages/libraries for basic HTTP authentication and JWT authentication with Passport.
    -Created “passport.js" file with  authentication strategies.
    -Created “auth.js" file with "/login” endpoint.
    -Updated endpoints in “index.js" file.

--- index.js
+++ index.js
@@ -17,10 +17,13 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+let auth = require('./auth')(app);
+const passport = require('passport');require('./passport');
+
 app.use(morgan("common"));
 
 //Mongoose - Get all movies and all their data (2a)
-app.get('/movies', async (req, res) => {
+app.get('/movies', passport.authenticate('jwt', { session: false }), async (req, res) => {
   await Movies.find()
   .then((movie) => {
     res.status(200).json(movie);
@@ -32,7 +35,7 @@ app.get('/movies', async (req, res) => {
 });
 
 //Mongoose - Get movie by title (2b)
-app.get('/movies/:Title', async (req, res) => {
+app.get('/movies/:Title', passport.authenticate('jwt', { session: false }), async (req, res) => {
   await Movies.findOne({ Title: req.params.Title })
     .then((movie) => {
       res.status(200).json(movie);
@@ -55,8 +58,8 @@ app.get('/movies/:Title', async (req, res) => {
   }
 });*/
 
-//Mongoose - Get genre by genre name (2c) "CODE NOT WORKING - error 404"
-app.get("/movies/genre/:genreName", async (req, res) => {
+//Mongoose - Get genre by genre name (2c)
+app.get("/movies/genre/:genreName", passport.authenticate('jwt', { session: false }), async (req, res) => {
   console.log(req.params.genreName);
   await Movies.findOne({ "Genre.Name": req.params.genreName })
   .then((movie) => {
@@ -82,7 +85,7 @@ app.get("/movies/genre/:genreName", async (req, res) => {
 });*/
 
 //Mongoose - Get data about a director (2d)
-app.get('/movies/director/:directorName', async (req, res) => {
+app.get('/movies/director/:directorName', passport.authenticate('jwt', { session: false }), async (req, res) => {
   await Movies.findOne({ "Director.Name": req.params.directorName })
     .then((movie) => {
       res.status(200).json(movie.Director);
@@ -107,29 +110,29 @@ app.get('/movies/director/:directorName', async (req, res) => {
 
 //Mongoose - Add a user (2e)
 app.post('/users', async (req, res) => {
-    await Users.findOne({ Username: req.body.Username })
-      .then((user) => {
-        if (user) {
-          return res.status(400).send(req.body.Username + 'already exists');
-        } else {
-          Users
-            .create({
-              Username: req.body.Username,
-              Password: req.body.Password,
-              Email: req.body.Email,
-              Birthday: req.body.Birthday
-            })
-            .then((user) =>{ res.status(201).json(user) })
-          .catch((error) => {
-            console.error(error);
-            res.status(400).send('Error: Missing information & ' + error);
+  await Users.findOne({ Username: req.body.Username })
+    .then((user) => {
+      if (user) {
+        return res.status(400).send(req.body.Username + 'already exists');
+      } else {
+        Users
+          .create({
+            Username: req.body.Username,
+            Password: req.body.Password,
+            Email: req.body.Email,
+            Birthday: req.body.Birthday
           })
-        }
-      })
-      .catch((error) => {
-        console.error(error);
-        res.status(500).send('Error: ' + error);
-  });
+          .then((user) =>{res.status(201).json(user) })
+        .catch((error) => {
+          console.error(error);
+          res.status(500).send('Error: ' + error);
+        })
+      }
+    })
+    .catch((error) => {
+      console.error(error);
+      res.status(500).send('Error: ' + error);
+    });
 });
 
 //Module 2.2.5 Endpoints - Add a user (2e)
@@ -169,8 +172,11 @@ app.get('/users/:Username', async (req, res) => {
   });
 });
 
-//Mongoose - Allow users to update their info (2f) "NOT WORKING - Code 200 and Null response, and user parameters don't get updated"
-app.put('/users/:Username', async (req, res) => {
+//Mongoose - Allow users to update their info (2f)
+app.put('/users/:Username', passport.authenticate('jwt', { session: false }), async (req, res) => {
+  if(req.user.Username !== req.params.Username){
+    return res.status(400).send('Permission denied');
+  }
   await Users.findOneAndUpdate({ Username: req.params.Username },
   { $set:
     {
@@ -206,7 +212,10 @@ app.put('/users/:Username', async (req, res) => {
 });*/
 
 //Mongoose - Allow users to add movie to their favorites (2g)
-app.post('/users/:Username/movies/:MovieID', async (req, res) => {
+app.post('/users/:Username/movies/:MovieID', passport.authenticate('jwt', { session: false }), async (req, res) => {
+  if(req.user.Username !== req.params.Username){
+    return res.status(400).send('Permission denied');
+  }
   await Users.findOneAndUpdate({ Username: req.params.Username }, {
      $push: { FavoriteMovies: req.params.MovieID }
    },
@@ -237,7 +246,10 @@ app.post('/users/:Username/movies/:MovieID', async (req, res) => {
 });*/
 
 //Mongoose - Allow users to delete movie from their favorites (2h)
-app.delete('/users/:Username/movies/:MovieID', async (req, res) => {
+app.delete('/users/:Username/movies/:MovieID', passport.authenticate('jwt', { session: false }), async (req, res) => {
+  if(req.user.Username !== req.params.Username){
+    return res.status(400).send('Permission denied');
+  }
   await Users.findOneAndUpdate({ Username: req.params.Username }, {
      $pull: { FavoriteMovies: req.params.MovieID }
    },
@@ -270,7 +282,10 @@ app.delete('/users/:Username/movies/:MovieID', async (req, res) => {
 });*/
 
 //Mongoose - Delete user (2i)
-app.delete('/users/:Username', async (req, res) => {
+app.delete('/users/:Username', passport.authenticate('jwt', { session: false }), async (req, res) => {
+    if(req.user.Username !== req.params.Username){
+    return res.status(400).send('Permission denied');
+  }
   await Users.findOneAndDelete({ Username: req.params.Username })
     .then((user) => {
       if (!user) {
