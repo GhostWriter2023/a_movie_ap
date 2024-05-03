c216b520b972fe9b8458986b713d610620a2e725
GhostWriter2023 <154560842+GhostWriter2023@users.noreply.github.com>
2024-05-03 10:22:23 -0400

    Added a NEW Endpoint
    -Added GET all users.

--- index.js
+++ index.js
@@ -92,6 +92,18 @@ app.get('/movies/director/:directorName', passport.authenticate('jwt', { session
   });
 });
 
+//Mongoose - Get all users
+app.get('/users', passport.authenticate('jwt', { session: false }), async (req, res) => {
+  await Users.find()
+    .then((users) => {
+      res.status(200).json(users);
+    })
+    .catch((err) => {
+      console.error(err);
+      res.status(500).send('Error: ' + err);
+  });
+});
+
 //Mongoose - Add a user (2e)
 app.post('/users', [
   check('Username', 'Username must be at least 5 characters long').isLength({min: 5}),
