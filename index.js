bb0304cfe08ec9ff5de15dac8e4d4939476686d1
GhostWriter2023 <154560842+GhostWriter2023@users.noreply.github.com>
2024-07-01 07:57:51 -0400

    Update index.js
    -Changes to CORS

--- index.js
+++ index.js
@@ -21,21 +21,28 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 const cors = require('cors');
-let allowedOrigins = ['http://localhost:1234', 'https://myflix-app-2024.netlify.app', 'http://localhost:4200'];
+let allowedOrigins = [
+  'http://localhost:1234',
+  'https://myflix-app-2024.netlify.app',
+  'http://localhost:4200'];
 
-app.use(cors({
-  origin: (origin, callback) => {
-    if(!origin) return callback(null, true);
-    if(allowedOrigins.indexOf(origin) === -1){ // If a specific origin isn’t found on the list of allowed origins
-      let message = 'The CORS policy for this application doesn’t allow access from origin ' + origin;
-      return callback(new Error(message), false);
-    }
-    return callback(null, true);
-  },
-  methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+app.use(
+  cors({
+    origin: (origin, callback) => {
+      if(!origin) return callback(null, true);
+      if(allowedOrigins.indexOf(origin) === -1){ // If a specific origin isn’t found on the list of allowed origins
+        let message =
+        'The CORS policy for this application doesn’t allow access from origin ' +
+        origin;
+        return callback(new Error(message), false);
+      }
+      return callback(null, true);
+    },
+/*  methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
   allowedHeaders: 'Origin, X-Requested-With, Content-Type, Accept, Authorization',
-  credentials: true
-}));
+  credentials: true*/
+})
+);
 
 // Handling preflight requests
 app.options('*', cors({
